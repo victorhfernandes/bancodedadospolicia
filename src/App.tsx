0c73aa923a6ed584f51e40ea47c4970954204ff7
Victor Hugo Fernandes <victorh.fernandes.o@gmail.com>
2024-10-15 18:15:48 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About";
-import NavBar from "./components/NavBar/NavBar";
-import Footer from "./components/Footer/Footer";
 import "./App.css";
 
 const App = () => {
