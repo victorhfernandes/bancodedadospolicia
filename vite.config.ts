c3559ed7d42b21bf8c66c6df55bfb450e6155771
Victor Hugo Fernandes <victorh.fernandes.o@gmail.com>
2024-10-18 02:36:15 -0300

    change vite port

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 6, // Alterar a porta para 3000 ou qualquer outra que preferir
+  },
 });
